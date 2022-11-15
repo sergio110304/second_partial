1c89209e05c70ab14ce4aff2a49eab6c715d7538
sergio110304 <105168888+sergio110304@users.noreply.github.com>
2022-11-15 08:59:39 -0500

    Create abstract.py

---
+++ abstract.py
@@ -0,0 +1,22 @@
+from abc import ABC
+
+class Entrada_peli(ABC):
+
+    def selec_pelicula(self):
+        pass
+
+    def selec_hora(self):
+        pass
+
+    
+    def selec_cantidad(self):
+        pass
+
+    
+    def selec_asiento(self):
+        pass
+
+    
+    def comprar(self):
+        pass
+        
